0d72dbfd896a60e801b61d51b3d24577ab470831
notageek-guy <manishkumar97430@gmail.com>
2024-07-02 08:07:53 +0530

    re-init the project

--- client/src/lib/api.ts
+++
@@ -1,6 +0,0 @@
-
-
-import { hc } from "hono/client";
-
-
-import {type Apptype} from '@server/app'
--- server/routes/validate.ts
+++
@@ -1,5 +0,0 @@
-import { Hono } from "hono";
-
-export const validateRoute = new Hono().get("/validate", async (c) => {
-  return c.json({ name: "manish" });
-});
